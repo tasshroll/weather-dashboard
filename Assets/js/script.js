5e9116c2d98f95000b17d91769c021ce8662e5e2
Tifni Shroll <tasshroll@gmail.com>
2023-03-28 00:16:35 -0700

    fixed api call to get the current temp and added a weather icon

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -20,7 +20,9 @@ var formSubmitHandler = function (event) {
     getLatLong(cityToSearch);
 }
 
-
+////////////////////
+// getLatLong
+//
 //Call Open Weather with a City and get the lat and long coordinates
 //http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 var getLatLong = function (city) {
@@ -35,7 +37,7 @@ var getLatLong = function (city) {
                 response.json().then(function (data) {
                     console.log(data);
                     var output = '';
-                    //display the five city's found in Open Weather on the console
+                    //Log the first 5 city's found in Open Weather
                     for (let i = 0; i < data.length; i++) {
                         lat = data[i].lat;
                         long = data[i].lon;
@@ -43,20 +45,28 @@ var getLatLong = function (city) {
                         let shLong = long.toFixed(2);
                         let name = data[i].name;
                         let state = data[i].state;
-                        let country=data[i].country;
-                        // output += '<li>' + data[i].name + ', ' + data[i].country + ' Lat  ' + lat + ' Long ' + long + '</li>';
-                        // output += '<li>' + name + ', ' + state + ', ' + country + ' Lat  ' + shLat + ' Long ' + shLong + '</li>';
-                        console.log(name + ', ' + state + ', ' + country + ' Lat  ' + shLat + ' Long ' + shLong );
+                        let country = data[i].country;
+                        console.log(name + ', ' + state + ', ' + country + ' Lat  ' + shLat + ' Long ' + shLong);
                     }
-                    var currrentCityEl = document.getElementById('cities');
-                    console.log ("City to search forr is ", city);
+                    console.log("City to search for is ", city);
+
+
+                    //Clear weather icon
+                    var iconElement = document.getElementById("icon");
+                    var iconImgEl = iconElement.querySelector("img");
+                    if (iconElement.contains(iconImgEl)) {
+                        iconElement.removeChild(iconImgEl); //remove child from the img element
+                    }
+
+                    // Print City, State and Date on Web Page
+                    let today = dayjs();
+                    let dateOutput = today.format('M/D/YYYY');
                     let state = data[0].state;
                     let cityOutput = city + ", " + state;
-                    console.log (cityOutput);
-                    currrentCityEl.innerHTML = cityOutput;
-                    console.log("currrentCityEl is", currrentCityEl);
-                    // Retreive weather for the first City in the returned list using lat and long
-                    
+                    var currrentCityEl = document.getElementById('cities');
+                    currrentCityEl.innerHTML = cityOutput + " (" + dateOutput + ")";
+
+                    //Get weather using first City in API response
                     lat = data[0].lat;
                     long = data[0].lon;
                     getWeatherApi(lat, long);
@@ -70,61 +80,75 @@ var getLatLong = function (city) {
         });
 }
 
+////////////////////
+// getWeatherApi
+// called by getLatLong
 // Call OpenWeather API  and pass in the lat long from first city
 var getWeatherApi = function (lat, long) {
     console.log("Inside getWeatherApi fetching weather for lat-long of ", lat, long);
     // insert lat long into API
     //api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
     var units = "imperial";
-    var querySelectors = '&lat=' + lat +
-'&lon=' + long + '&appid=' + apiKey + '&units=' + units;
-    var currentWeatherApiUrl = 'https://api.openweathermap.org/data/2.5/weather?' + querySelectors;
+    var querySelectors = '&lat=' + lat + '&lon=' + long + '&appid=' + apiKey + '&units=' + units;
+    var currWeatherApiUrl = 'https://api.openweathermap.org/data/2.5/weather?' + querySelectors;
 
     //https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
     var forecastApiUrl = 'https://api.openweathermap.org/data/2.5/forecast?' + querySelectors;
     // var forecastApiUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat +
     //     '&lon=' + long + '&appid=' + apiKey + '&units=' + units;
     // var apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat='+{lat}+'&lon='+{long}+'&appid='+{apiKey}+'&units='+{units};
-   
-    console.log("Current Weather API URL is ", currentWeatherApiUrl)
-    console.log("Forecast API URL is ", forecastApiUrl)
 
-    fetch(forecastApiUrl)
+    console.log("Current Weather API URL is ", currWeatherApiUrl);
+    console.log("Forecast API URL is ", forecastApiUrl);
+
+    fetch(currWeatherApiUrl)
         .then(function (response) {
             if (response.ok) {
-                console.log(response);
                 response.json().then(function (data) {
-                    console.log(data);
-                    // Display the first city temperature
-                    var outputTemp = '';
-                    var city = data.city.name;
-                    let cityTemp = data.list[0].main.temp;
-                    let windSpeed = data.list[0].wind.speed;
-                    let humidity = data.list[0].main.humidity;
-                    let date = data.list[0];
-                    // outputTemp += '<span>' + cityTemp + '</span>';
-                    console.log ("Date is ", date);
-                    console.log("Temperature is ", cityTemp);
-                    console.log("Windspeed is ", windSpeed);
-                    console.log("Humidity is ", humidity);
 
+                    console.log("Current weather conditions for Current City is", data);
+                    // Display Icon by City and Date
+
+
+
+
+
+
+
+
+
+
+                    // Display the current temperature for city
+                    var outputTemp = '';
+                    var city = data.name;
+                    let cityTemp = data.main.temp;
+                    let windSpeed = data.wind.speed;
+                    let humidity = data.main.humidity;
+                    let date = data.dt_text;
+                    let icon = data.weather[0].icon;
+
+                    // outputTemp += '<span>' + cityTemp + '</span>';
+                    console.log("Date: ", date, "City: ", city, " curr temp: ", cityTemp, " curr wind : ", windspeed, " curr humidity:", humidity);
+
+                    //Query Selectors
                     var temperatureEL = document.getElementById("temperature");
                     var windSpeedEL = document.getElementById("windspeed");
                     var humidityEL = document.getElementById("humidity");
 
-                    temperatureEL.innerHTML = "Temperature: " + cityTemp + '\u00B0F';                    
+                    //Display icon
+                    let iconDisplay = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
+                    console.log("Icon is ", iconDisplay);
+                    var iconElement = document.getElementById("icon");
+                    const iconImg = document.createElement("img");
+                    iconImg.setAttribute("src", iconDisplay);
+                    iconElement.appendChild(iconImg);
+
+
+
+                    temperatureEL.innerHTML = "Temperature: " + cityTemp + '\u00B0F';
                     windSpeedEL.innerHTML = "Windspeed: " + windSpeed + " MPH";
                     humidityEL.innerHTML = "Humidity: " + humidity + "%";
-                    // Get the 5 day forecast
-                    for (let i=0; i<data.list.length; i+=8) {
-                        // the forecast data is for every 24 hours in 3 hour increments
-                        // grab the first data point out of the 8
-        
-                        let forecastTemp = data.list[i].main.temp_max;
-                        let date = data.list[i].dt_txt;
 
-                        console.log ("Future forecasted high temp for ", date," is ", forecastTemp);
-                    }
                 });
             } else {
                 alert('Error: ' + response.statusText);
@@ -133,7 +157,53 @@ var getWeatherApi = function (lat, long) {
         .catch(function (error) {
             alert('Unable to connect to OpenWeather');
         });
-};
+
+
+
+    fetch(forecastApiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    console.log(data);
+                    // Display the first city temperature
+                    // var outputTemp = '';
+                    // var city = data.city.name;
+                    // let cityTemp = data.list[0].main.temp;
+                    // let windSpeed = data.list[0].wind.speed;
+                    // let humidity = data.list[0].main.humidity;
+                    // let date = data.list[0];
+                    // // outputTemp += '<span>' + cityTemp + '</span>';
+                    // console.log("Date is ", date);
+                    // console.log("Temperature is ", cityTemp);
+                    // console.log("Windspeed is ", windSpeed);
+                    // console.log("Humidity is ", humidity);
+
+                    // var temperatureEL = document.getElementById("temperature");
+                    // var windSpeedEL = document.getElementById("windspeed");
+                    // var humidityEL = document.getElementById("humidity");
+
+                    // temperatureEL.innerHTML = "Temperature: " + cityTemp + '\u00B0F';
+                    // windSpeedEL.innerHTML = "Windspeed: " + windSpeed + " MPH";
+                    // humidityEL.innerHTML = "Humidity: " + humidity + "%";
+                    // Get the 5 day forecast
+                    for (let i = 3; i < data.list.length; i += 8) {
+                        // the forecast data is for every 24 hours in 3 hour increments
+                        // grab the 4th data point out of the 8 to get temperature at 3PM
+                        let forecastTemp = data.list[i].main.temp;
+                        let date = data.list[i].dt_txt;
+                        console.log("Future forecasted high temp for ", date, " is ", forecastTemp);
+                    }
+                });
+
+            } else {
+                alert('Error: ' + response.statusText);
+            }
+        })
+        .catch(function (error) {
+            alert('Unable to connect to OpenWeather');
+        });
+} // END of getWeatherApi
+
 
 // Get user input for a City
 formUserEl.addEventListener('submit', formSubmitHandler);
